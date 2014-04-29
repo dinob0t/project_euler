23bec061085d949b5c37914de6d7cf8372858115
Dean Hillan <dean.hillan@gmail.com>
2014-04-29 21:33:21 +1000

    24

--- 24.py
+++ 24.py
@@ -2,10 +2,11 @@
 
 def lexicographic_order_to_num(digits, count_num):
 	current = range(0,digits+1)
+	print current
 	count = 1
 	if count_num == 1:
 		return current
-	while count <=count_num:
+	while count <count_num:
 
 		for i in range(0,digits):
 			if current[i] < current[i+1]:
@@ -16,16 +17,21 @@ def lexicographic_order_to_num(digits, count_num):
 		tmp = current[l]
 		current[l] = current[k]
 		current[k] = tmp
+		#print current, 'before', k+1, digits
 
-		current[k+1:digits] in reversed(current[k+1:digits+1])
+		for i in range(k+1, digits):
+			new_end = current.pop(k+1)
+			#print new_end, digits-1-(i-k-1)
+			current.insert(digits - (i-k-1) ,new_end)
 
 
 
-		print current
+
+		#print current
 		count = count + 1
 	return current
  
 
 
 if __name__ == "__main__":
-	lexicographic_order_to_num(2, 3)
+	print lexicographic_order_to_num(9,1000000)
