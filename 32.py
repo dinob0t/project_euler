64224a85ddb2bf9f7029cb2ca818b4e2e461a350
Dean Hillan <dean.hillan@gmail.com>
2014-05-11 17:05:26 +1000

    update

--- 32.py
+++ 32.py
@@ -1,16 +1,25 @@
-import math
+import itertools
+
 
 def pandigitals(n):
-	pan_dict = {}
-	digit_list = range(1,n+1)
-	n_str = str(n)
-	max_n = int('9'*len(n_str))
-	max_half = len(n_str)//2
+	pan_digit_prods = []
+	perms = (itertools.permutations(range(1,n+1),n))
+	for perm in perms:
+		for n1 in range(n-1):
+			for n2 in range(n1+1,n-1):
+				perm_list = map(str,list(perm))
+				num1 = int(''.join(perm_list[0:n1+1]))
+				num2 = int(''.join(perm_list[n1+1:n2+1]))
+				eq = int(''.join(perm_list[n2+1:n+1]))
+				
+				if num1*num2 == eq:
+					if eq not in pan_digit_prods:
+						pan_digit_prods.append(eq)
+					
 
-	for i in range(1,max_n+1):
-		for j in range(1,max_n+1):
-			#go backwards from number and find divisors
+	return sum(pan_digit_prods)
+
 
 
 if __name__ == "__main__":
-	print pandigitals(2)
+	print pandigitals(9)
