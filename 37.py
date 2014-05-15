7106cdc504c8ecc47780795404a17d984f9a0dc2
dinob0t <dean.hillan@gmail.com>
2014-05-15 17:45:23 +1000

    37!

--- 37.py
+++ 37.py
@@ -3,7 +3,7 @@ def isprime(n):
 	if n<=0:
 		return False
 	if n==1:
-		return True
+		return False
 	if n ==2:
 		return True
 	if n%2 == 0:
@@ -13,55 +13,34 @@ def isprime(n):
 			return False
 	return True
 
-def find_truncable_primes():
-	starting_primes = [2,3,5,7]
-	for start in starting_primes:
-		expand_prime(start)
-
-def expand_prime(start):
-	print start
-	for right in range(1,10):
-		join = int(str(start) + str(right))
-		if check_left_right(join):
-			print 'passed check, expanding'
-			expand_prime(join)	
-		else:
-			if start not in trun_list and len(str(start)) > 1:
-				print 'adding', start
-				trl = len(trun_list)
-				if trl == 0:
-					trun_list.append(start)
-				elif start in trun_list:
-					pass
-				else:
-					trun_str = list(str(trun_list[trl-1]))
-
-					if trl> 
-
-					print trun_str, trun_list, start
-				 	if trl > len(str(start)) and int("".join(trun_str[:len(trun_str)-2])) == start:
-			 			pass
-		 			else:
-						trun_list.append(start)
+def generate_trun_list(num, direc):
+	trunc_list = [2,3,5,7]
+	
+	for current in trunc_list:
+		if max(trunc_list) > num:
+			return trunc_list
+		for i in range(1,10):
+			if direc == 'r':
+				join = int(str(current) + str(i))
+			else:
+				join = int(str(i) + str(current))
+			if isprime(join) and join not in trunc_list:
+				trunc_list.append(join)
 
 
-
-def check_left_right(num):
-	if len(str(num))==1:
-		return True
-	if isprime(num):
-		num_str = list(str(num))
-		print 'made it right'
-		if isprime(int("".join(num_str[1:]))):
-			print 'made it left'	
-			return True	
-	return False
-			
-
-
+def join_lists(rl, ll):
+	both_list = []
+	for r in rl:
+		if r in ll and len(str(r))>1:
+			both_list.append(r)
+	return both_list
 
 if __name__ == "__main__":
-	trun_list = []
-	find_truncable_primes()
-	print trun_list
+	right_list =  generate_trun_list(1000000,'r')
+	print right_list
+	left_list =  generate_trun_list(1000000,'l')
+	print left_list
+	both = join_lists(right_list, left_list)
+	print both
+	print sum(both)
 	
