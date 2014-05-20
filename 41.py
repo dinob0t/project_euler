cdb494038f4d85e5c0af7b12db4ff119214993be
dinob0t <dean.hillan@gmail.com>
2014-05-20 14:58:12 +1000

    41

---
+++ 41.py
@@ -0,0 +1,30 @@
+import itertools
+import math
+
+def pandigitals(n):
+	pan_digit_prods = []
+	perms = (itertools.permutations(range(1,n+1),n))
+	for perm in perms:
+		current = int("".join(map(str,list(perm))))
+		
+		if isprime(current):
+			print current
+
+	return 0
+
+def isprime(n):
+	if n<=0:
+		return False
+	if n==1:
+		return False
+	if n ==2:
+		return True
+	if n%2 == 0:
+		return False
+	for i in range(3,int(math.sqrt(n))+1):
+		if n%i == 0:
+			return False
+	return True
+
+if __name__ == "__main__":
+	print pandigitals(7)
