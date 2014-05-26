c5c44a65786f4369bad00807de8c571bf3dd7f03
dinob0t <dean.hillan@gmail.com>
2014-05-27 09:50:43 +1000

    46!

--- 46.py
+++ 46.py
@@ -33,7 +33,29 @@ def generate_twice_squares(n_max):
 		cur = num**2
 	return twice_list
 
+def generate_odd_composite(n_max):
+	comp_list = []
+	cur = 9
+	while cur<n_max:
+		if not isprime(cur):
+			comp_list.append(cur)
+		cur += 2
+	return comp_list
+
+def test_goldbach(p_l, t_l, c_l):
+	for prime in p_l:
+		for twice in t_l:
+			cur = prime + twice
+			if cur in c_l:
+				c_l.remove(cur)
+	return c_l
+
+
+
 
 if __name__ =="__main__":
-	print generate_primes(100)
-	print generate_twice_squares(100)
+	num = 10000
+	prime_list = generate_primes(num)
+	twice_list = generate_twice_squares(num)
+	comp_list =  generate_odd_composite(num)
+	print test_goldbach(prime_list, twice_list, comp_list)
